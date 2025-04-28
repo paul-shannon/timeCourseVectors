6cd1bd9104aab8820603e0b07e597c1723a2538f
paul-shannon <paul.thurmond.shannon@gmail.com>
2025-04-27 17:12:50 -0700

    now testing out 200k around KLF1 tss

--- R/TimeCourseVectors.R
+++ R/TimeCourseVectors.R
@@ -6,6 +6,9 @@
 #' @rdname TimeCourseVectors
 #' @aliases TimeCourseVectors
 #----------------------------------------------------------------------------------------------------
+#' @import GenomicRanges
+#' @import igraph
+#----------------------------------------------------------------------------------------------------
 #' @description
 #' An R6 class to transform experimental matrix data into timecourse vectors.
 #'
@@ -17,7 +20,12 @@ TimeCourseVectors = R6Class("TimeCourseVectors",
 
     #--------------------------------------------------------------------------------
     private = list(tbl=data.frame(),
-                   scoreColumn=NULL
+                   scoreColumn=NULL,
+                   tbl.ov=data.frame(),
+                   xtab=NULL,
+                   families=NULL,
+                   parents=c(),
+                   orphans=c()
                    ),
 
     #--------------------------------------------------------------------------------
@@ -38,7 +46,115 @@ TimeCourseVectors = R6Class("TimeCourseVectors",
         #' @return the current value of the id member
         getScoreColumn = function(){
             private$scoreColumn
-            }
+            },
+        #------------------------------------------------------------
+        #' @description accessor for the object's data.frame
+        #' @return current base table of genomic regions
+        getTable = function(){
+            private$tbl
+            },
+        #------------------------------------------------------------
+        #' @description find location-sharing genomic regions,
+        #' each of which we regard as a family of regions, with
+        #' the specified minimum overlap.  singleton families are
+        #' included.
+        #' @param miniOverlap integer, the number of shared bases
+        #' @return the current value of the id member
+        findRegionFamilies = function(minOverlap=20){
+            ov <- findOverlaps(GRanges(private$tbl),
+                               GRanges(private$tbl), type="within")
+            tbl.ov <- as.data.frame(ov)
+            tbl.ov <- subset(tbl.ov, queryHits != subjectHits)
+            g <- graph_from_data_frame(tbl.ov)
+               # produces 4 isolated subgraphs
+               # extract them.  these are families. display in igv
+               #
+            all <- sort(unique(with(tbl.ov, c(queryHits, subjectHits))))
+            private$orphans <- setdiff(seq_len(nrow(private$tbl)), all)
+            private$tbl.ov <- tbl.ov
+            xtab <- table(with(tbl.ov, c(queryHits, subjectHits)))
+            tbl.dist <- as.data.frame(xtab)
+            colnames(tbl.dist) <- c("row", "count")
+            tbl.dist <- tbl.dist[rev(order(tbl.dist$count)),]
+            rownames(tbl.dist) <- NULL
+            membership <- components(g)$membership
+            components <- sort(unique(as.integer(components(g)$membership)))
+            clusters <- list()
+            for(component in components){
+               clusters[[component]] <- as.integer(names(membership[membership==component]))
+               } # for component
+            private$families <- clusters
+            }, # findRegionFamilies
+
+        #------------------------------------------------------------
+        #' @description accessor for the object's GRanges overlap
+        #' @return the overlap table as a data.frame
+        getOverlaps = function(){
+            private$tbl.ov
+            },
+
+        #------------------------------------------------------------
+        #' @description accessor for the object's GRanges overlap
+        #' @return the overlap table as a data.frame
+        getFamilySizes = function(){
+            as.integer(lapply(private$clusters, length))
+            },
+
+
+        #------------------------------------------------------------
+        #' @description accessor for the parents of all the grouped regions
+        #' @return the parents, a list
+        getFamilyCount = function(){
+            return(length(private$families))
+            },
+
+        #------------------------------------------------------------
+        #' @description accessor for the parents of all the grouped regions
+        #' @return the parents, a list
+        getFamily = function(which){
+            return(sort(unlist(private$families[which])))
+            },
+
+        #------------------------------------------------------------
+        #' @description accessor for the parents of all the grouped regions
+        #' @return the parents, a list
+        getOrphans = function(){
+            return(private$orphans)
+            },
+
+        #------------------------------------------------------------
+        #' @description accessor for the parents of all the grouped regions
+        #' @return the parents, a list
+        displayFamily = function(which){
+            rows <- unlist(private$families[which])
+            maxVal <- 1.1 * max(tbl[, private$scoreColumn])
+            tbl.fam <- private$tbl[rows,]
+            for(i in seq_len(nrow(tbl.fam))){
+               trackName <- sprintf("%d-%d-%d-%d", i, tbl$day[i], tbl$rep[i], which)
+               tbl.track <- tbl.fam[i, c("chrom", "start", "end", private$scoreColumn)]
+               track <- DataFrameQuantitativeTrack(trackName, tbl.track,
+                                        color="black", autoscale=FALSE, min=0, max=maxVal)
+               displayTrack(igv, track)
+               } # for i
+            }, # displayFamilies
+
+        #------------------------------------------------------------
+        #' @description use igv to display tracks, consolidated by day
+        #' @return nothing
+        displayByDay = function(){
+            tbl <- private$tbl
+            maxVal <- 1.1 * max(tbl[, private$scoreColumn])
+            days <- sort(unique(tbl$day))
+            for(Day in days){
+               trackName <- sprintf("day %d", Day)
+               tbl.track <- subset(tbl, day==Day)[, c("chrom", "start", "end", private$scoreColumn)]
+               track <- DataFrameQuantitativeTrack(trackName, tbl.track,
+                                        color="brown", autoscale=FALSE, min=0, max=maxVal)
+               displayTrack(igv, track)
+               } # for day
+            } # displayByDay
+
+        #------------------------------------------------------------
        ) # public
 
     ) # class
--- inst/unitTests/test_TimeCourseVectors.R
+++ inst/unitTests/test_TimeCourseVectors.R
@@ -1,21 +1,29 @@
+if("TimeCourseVectors" %in% .packages())
+   detach("package:TimeCourseVectors", unload=TRUE)
 library(RUnit)
 library(TimeCourseVectors)
 #----------------------------------------------------------------------------------------------------
-runTests <- function()
-{
-    test_ctor()
+# prepared in
+# ~/github/TrenaProjectErythropoiesis/inst/extdata/genomicRegions/atacPrep.R
 
-} # runTests
+f <- system.file(package="TimeCourseVectors", "extdata", "klf1-atac-10k.RData")
+
+tbl <- get(load(f))
+checkEquals(dim(tbl), c(28, 8))
+requiredColumns <- c("chrom", "start", "end", "rep", "day")
+checkTrue(all(requiredColumns %in% colnames(tbl)))
+
+f2 <- system.file(package="TimeCourseVectors", "extdata", "klf1-atac-200k.RData")
+tbl2 <- get(load(f2))
+# checkEquals(dim(tbl), c(28, 8))
+requiredColumns <- c("chrom", "start", "end", "rep", "day")
+checkTrue(all(requiredColumns %in% colnames(tbl2)))
+
 #----------------------------------------------------------------------------------------------------
 test_ctor <- function()
 {
     message(sprintf("--- test_ctor"))
 
-    f <- system.file(package="TimeCourseVectors", "extdata", "klf1-atac-10k.RData")
-    tbl <- get(load(f))
-    checkEquals(dim(tbl), c(28, 8))
-    requiredColumns <- c("chrom", "start", "end", "rep", "day")
-    checkTrue(all(requiredColumns %in% colnames(tbl)))
     scoreColumn <- "fc"
     tcv <- TimeCourseVectors$new(tbl, scoreColumn)
     checkTrue(all(c("R6", "TimeCourseVectors") %in% class(tcv)))
@@ -24,5 +32,71 @@ test_ctor <- function()
 
 } # test_ctor
 #----------------------------------------------------------------------------------------------------
-if(!interactive())
+# small test to start: just 10 regions, 2 of them (4 and 7) are singletons
+test_findRegionFamilies <- function()
+{
+    printf("--- test_findRegionFamilies")
+    scoreColumn <- "fc"
+    scoreColumn <- "qvalScore"
+    tbl.sub <- head(tbl, n=10)
+    tcv <- TimeCourseVectors$new(tbl, scoreColumn)
+    tcv$findRegionFamilies(minOverlap=100)
+
+    tbl.ov <- tcv$getOverlaps()
+    checkEquals(tcv$getFamilyCount(), 4)
+    checkEquals(tcv$getFamily(1), c(1,3,6,8,11,14,17,20,24,28))
+    checkEquals(tcv$getFamily(2), c(2,5,10,13,16,19,23,27))
+    checkEquals(tcv$getFamily(3), c(12,15,18,21,25))
+    checkEquals(tcv$getFamily(4), c(22,26))
+    checkEquals(tcv$getOrphans(), c(4,7,9))
+      #  all of these together should be the same as all rows
+    checkEquals(sort(c(tcv$getOrphans(),
+                       tcv$getFamily(1),
+                       tcv$getFamily(2),
+                       tcv$getFamily(3),
+                       tcv$getFamily(4))), seq_len(nrow(tbl)))
+
+    browser()
+    # igv <<- start.igv("KLF1")
+    tcv$displayFamily(1)
+    tcv$displayFamily(2)
+    tcv$displayFamily(3)
+    tcv$displayFamily(4)
+    tcv$displayByDay()
+
+    #
+    #checkEquals(parents, c(1,2,3,5,6,8,10))
+    browser()
+    xyz <- 99
+
+
+} # test_findRegionFamilies
+#----------------------------------------------------------------------------------------------------
+# small test to start: just 10 regions, 2 of them (4 and 7) are singletons
+test_findRegionFamilies_200k <- function()
+{
+    printf("--- test_findRegionFamilies_200k")
+    scoreColumn <- "fc"
+    scoreColumn <- "qvalScore"
+    tcv <- TimeCourseVectors$new(tbl2, scoreColumn)
+    tcv$findRegionFamilies(minOverlap=100)
+    #tbl.ov <- tcv$getOverlaps()
+    checkEquals(tcv$getFamilyCount(), 31)
+    for(fam in seq_len(31)){
+       printf("    %d: %d", fam, length(tcv$getFamily(fam)))
+       }
+
+
+
+} # test_findRegionFamilies
+#----------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+    test_ctor()
+    #test_findRegionFamilies()
+    test_findRegionFamilies_200k()
+
+} # runTests
+#----------------------------------------------------------------------------------------------------
+# if(!interactive())
     runTests()
